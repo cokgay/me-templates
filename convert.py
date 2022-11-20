eeb6462eea5fe25827b2028e1a4c5a196a37a9c4
cart96 <agolfcart@tuta.io>
2022-11-20 17:49:21 +0300

    add html to rust format

---
+++ convert.py
@@ -0,0 +1,18 @@
+import os
+
+input_dir = "./templates"
+output_dir = "./generated"
+
+if os.path.exists(output_dir):
+    os.system(f"rm -rf {output_dir}")
+
+os.makedirs(output_dir)
+
+for file in os.listdir(input_dir):
+    with open(f"./{input_dir}/{file}", "r") as f:
+        content = f.read()
+
+    new_content = content.replace("{", "{{").replace("}", "}}").replace("{{0}}", "{0}").replace("{{1}}", "{1}").replace("{{2}}", "{2}").replace("{{3}}", "{3}").replace("\n", "")
+
+    with open(f"./{output_dir}/{file.replace('.html', '.temp')}", "w") as f:
+        f.write(new_content)
